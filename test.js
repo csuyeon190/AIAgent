7aec10b7ec237f600544eeb423f032b9baac419c
csuyeon190 <105414191+csuyeon190@users.noreply.github.com>
2026-07-06 12:44:55 +0900

    test.js commit

---
+++ test.js
@@ -0,0 +1,16 @@
+"use strict";
+
+
+var proto = {
+    bar: "bar",
+    foo: "foo"
+},
+    instance = Object.create(proto);
+
+proto.bar = "qux";
+instance.foo = "baz";
+//instance.bar = "qux";
+
+
+console.log("instance: " + JSON.stringify(instance));
+console.log("proto: " + JSON.stringify(proto));
